3e9c96b8a8f25d2941eba59ab70986135bb170d1
= <=>
2022-08-15 12:14:53 -0400

    Screens step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { NgxsLoggerPluginModule } from '@ngxs/logger-plugin';
 import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { PureInboxScreenComponent } from './components/pure-inbox-screen/pure-inbox-screen.component';
+import { InboxScreenComponent } from './components/inbox-screen/inbox-screen.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, PureInboxScreenComponent, InboxScreenComponent],
   imports: [
     BrowserModule,
     TaskModule,
---
+++ src/app/components/pure-inbox-screen/pure-inbox-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pure-inbox-screen',
+  templateUrl: './pure-inbox-screen.component.html',
+  styleUrls: ['./pure-inbox-screen.component.css']
+})
+export class PureInboxScreenComponent {
+
+  @Input() error: any;
+
+}
